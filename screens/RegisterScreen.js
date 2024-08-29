429d394bdf4cfde4f122f0131a948c0fe4c5e491
aidenkim08 <90583080+aidenkim08@users.noreply.github.com>
2024-08-28 20:36:29 -0700

    Update RegisterScreen.js

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -3,7 +3,8 @@ import {Text, TextInput,Button} from 'react-native-paper'
 import React, {useState, useEffect} from 'react'
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import {db, auth} from '../firebase'
-import {collection, setDoc, doc, getDocs } from "firebase/firestore"; 
+import {collection, setDoc, doc, getDocs, query} from "firebase/firestore"; 
+import { Picker } from '@react-native-picker/picker';
 
 
 
@@ -15,15 +16,41 @@ const RegisterScreen = ({navigation}) => {
     const [address, setAddress] = useState("")
     const [city, setCity] = useState("")
     const [zip, setZip] = useState("")
-
+    const [schools, setSchools] = useState([])
+    const [school, setSchool] = useState(" ")
     
+
+    useEffect(() => {
+        const fetchSchools = async () => {
+          try {
+            const querySnapshot = await getDocs(collection(db, "schools"));
+            const docsData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+            setSchools(docsData)
+          } catch (error) {
+            console.error("Error fetching schools: ", error);
+          }
+        };
+    
+        fetchSchools();
+      }, []);
     const signUp = async () => {
+        console.log("sign up started")
         createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed up 
             const userId = userCredential.user.uid;
-            // Add user data to Firestore
-            addUser(userId)
+            console.log(userId)
+            setDoc(doc(db, 'students', userId), {
+                //schoolName:schoolName,
+                schoolID:school,
+                name: name,
+                address: address,
+                city: city,
+                zip: zip,
+                phone: phone,
+                email: email
+            });
+            
             navigation.navigate('Login')
             
         })
@@ -33,28 +60,23 @@ const RegisterScreen = ({navigation}) => {
             console.log(errorMessage)
         });
     }
-
-    const addUser = async (userId) => { 
-        try{
-        await setDoc(doc(db, "drivers", userId), {
-            name,
-            email,
-            phone,
-            address,
-            city,
-            zip
-          });
-
-
-      console.log("Document written with ID: ", userId);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  }
-
+     
 
   return (
     <SafeAreaView style={{flex:1, padding:15,}}>
+    {/* School Picker */}
+<View style={{ marginVertical: 10 }}>
+<Picker
+  selectedValue={school}
+  onValueChange={(itemValue, itemIndex) => setSchool(itemValue)}
+>
+    {/* Placeholder Item */}
+    <Picker.Item label="Select School" value={null} />
+  {schools.map((school) => (
+    <Picker.Item key={school.id} label={school.name} value={school.id} />
+  ))}
+</Picker>
+</View>
       <TextInput style={styles.input} label="Email" value={email} onChangeText={setEmail} autoCapitalize='none'/>
       <TextInput style={styles.input} label="Password" value={password} onChangeText={setPassword} secureTextEntry autoCapitalize='none'/>
       <TextInput style={styles.input} label="Name" value={name} onChangeText={setName} />
